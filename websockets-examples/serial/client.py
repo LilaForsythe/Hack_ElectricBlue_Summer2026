517eb6e9e706df717ee8762ab43050152dae34d6
LilaForsythe <l4sythe99@gmail.com>
2026-08-09 11:31:20 -0700

    changed ip

--- websockets-examples/serial/client.py
+++ websockets-examples/serial/client.py
@@ -7,7 +7,7 @@ from datetime import datetime
 # Configuration
 SERIAL_PORT = 'COM7'  # Change to 'COM3' on Windows, '/dev/ttyUSB0' for other boards
 BAUD_RATE = 115200
-WEBSOCKET_URL = 'ws://localhost:8765'
+WEBSOCKET_URL = 'ws://192.168.0.105'
 
 
 async def read_serial_and_send():
